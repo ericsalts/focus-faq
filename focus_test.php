b761ca7dd00b2945f172430d66cec9c8f61dba40
[ericsalts] <eric.saltsman@polk-fl.net>
2018-05-01 15:31:42 -0400

    update

--- focus_test.php
+++ focus_test.php
@@ -5,12 +5,15 @@ include('pg_connect.php');
 
     $result = pg_query($db_connection, "SELECT * FROM schools LIMIT 1");
 
-    $arr = pg_fetch_all($result);
+    $arr =  pg_fetch_all($result);
 
+    print "<PRE>";
     print_r($arr);
+    print "</PRE>";
 
-    #print phpinfo();
     
+    #print phpinfo();
+
 
 
 ?>
